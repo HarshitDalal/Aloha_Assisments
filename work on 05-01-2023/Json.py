bdabd824fc5a3603fafe57d7a71bae09b81570ba
Harshit Dalal <dgmarket11@gmail.com>
2023-01-05 18:16:53 +0530

    JavaScript Object Notation (JSON) in Python

---
+++ work on 05-01-2023/Json.py
@@ -0,0 +1,26 @@
+import json
+
+if __name__ == '__main__':
+    data = {
+        'name': 'Harshit',
+        'age': 23,
+        'edu': [
+            {
+                'Graduation': 'BCA'
+            },
+            {
+                'Post Graduation': 'MCA'
+            }
+        ],
+        'work': 'Aloha Technology',
+        'married': False,
+        'disability': None
+    }
+
+    json_data = json.dumps(data, indent=4)
+    print(json_data)
+    print(type(json_data))
+
+    json_to_normal = json.loads(json_data)
+    print(json_to_normal)
+    print(type(json_to_normal))
